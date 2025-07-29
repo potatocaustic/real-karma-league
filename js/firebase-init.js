70b6e5516cc67c50bca4276d15d6040f123f665d
potatocaustic <causticfbg@gmail.com>
2025-07-28 22:22:07 -0500

    firebase init fix

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -2,33 +2,34 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut, connectAuthEmulator } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { 
-    getFirestore, 
-    collection, 
-    doc, 
-    getDoc, 
-    getDocs, 
-    query, 
-    where, 
-    limit, 
-    setDoc, 
-    deleteDoc, 
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    query,
+    where,
+    limit,
+    setDoc,
+    deleteDoc,
     serverTimestamp,
     addDoc,
     updateDoc,
+    writeBatch, // <--- ADDED: Import writeBatch
     connectFirestoreEmulator
 } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 import { getFunctions, httpsCallable, connectFunctionsEmulator } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-functions.js";
 
 // Your web app's Firebase configuration...
 const firebaseConfig = {
-  apiKey: "AIzaSyDch0dQ1c9_mDzANAvfMoK1HAnMrRl1WnY",
-  authDomain: "real-karma-league.firebaseapp.com",
-  projectId: "real-karma-league",
-  storageBucket: "real-karma-league.firebasestorage.app",
-  messagingSenderId: "158995195520",
-  appId: "1:158995195520:web:0e12dd5095595c0a42e865",
-  measurementId: "G-E8LNVNG5M1"
+    apiKey: "AIzaSyDch0dQ1c9_mDzANAvfMoK1HAnMrRl1WnY",
+    authDomain: "real-karma-league.firebaseapp.com",
+    projectId: "real-karma-league",
+    storageBucket: "real-karma-league.firebasestorage.app",
+    messagingSenderId: "158995195520",
+    appId: "1:158995195520:web:0e12dd5095595c0a42e865",
+    measurementId: "G-E8LNVNG5M1"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -63,5 +64,6 @@ export {
     deleteDoc,
     serverTimestamp,
     addDoc,
-    updateDoc
+    updateDoc,
+    writeBatch // <--- ADDED: Export writeBatch
 };
