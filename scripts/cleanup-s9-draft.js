4c9b1b310a4e1e69d4272003c0b76a6d6a969fd9
potatocaustic <causticfbg@gmail.com>
2025-11-25 11:10:12 -0600

    pfp fix

--- scripts/cleanup-s9-draft.js
+++ scripts/cleanup-s9-draft.js
@@ -2,7 +2,7 @@
 // This script deletes incorrect draft results and any created players
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
